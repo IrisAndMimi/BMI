c3c17d53068a1244d5039d71df157ce5ffd03b58
IrisAndMimi <my1ris227@outlook.com>
2017-10-24 11:53:41 +0800

    calculate bmi

--- app/src/main/java/com/irisandmimi/bmi/bmi.java
+++ app/src/main/java/com/irisandmimi/bmi/bmi.java
@@ -2,6 +2,10 @@ package com.irisandmimi.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 public class bmi extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class bmi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+    }
+    public void bmi(View view){
+//        System.out.println("What???");
+        Log.d("MainActivity","testing bmi method");
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+        float weight = Float.parseFloat(edWeight.getText().toString());
+        float height = Float.parseFloat(edHeight.getText().toString());
+        float bmi = weight/(height*height);
+        Log.d("MainActivity","Your bmi is"+bmi);
     }
 }
