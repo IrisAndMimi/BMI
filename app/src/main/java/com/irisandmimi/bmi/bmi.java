12b922224a49413155433582e6036399537c391d
IrisAndMimi <my1ris227@gmail.com>
2017-10-31 12:15:15 +0800

    輸出bmi值

--- app/src/main/java/com/irisandmimi/bmi/bmi.java
+++ app/src/main/java/com/irisandmimi/bmi/bmi.java
@@ -1,11 +1,13 @@
 package com.irisandmimi.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class bmi extends AppCompatActivity {
 
@@ -21,7 +23,14 @@ public class bmi extends AppCompatActivity {
         EditText edHeight = (EditText) findViewById(R.id.ed_height);
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
-        float bmi = weight/(height*height);
-        Log.d("MainActivity","Your bmi is"+bmi);
+        float bmi = weight / (height * height);
+        new AlertDialog.Builder(this)
+                .setMessage("Your bmi is "+ bmi)
+                .setTitle("Result")
+                .setPositiveButton("Okay",null)
+                .show();
+
+      /*  Log.d("MainActivity","Your bmi is "+ bmi);
+        Toast.makeText(this,"Your bmi is "+ bmi, Toast.LENGTH_LONG).show();*/
     }
 }
